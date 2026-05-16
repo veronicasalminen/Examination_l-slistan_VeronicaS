6f94321689decefd112f12a658725978d7e15974
Veronica <amaliasalminen@hotmail.com>
2026-05-16 17:04:57 +0200

    commit test failed

--- tests/unit/test_bookstore.py
+++ tests/unit/test_bookstore.py
@@ -1,9 +1,19 @@
 from backend.bookstore import BookStore
 
-
+# Testar att lägga till bok
 def test_add_book():
     store = BookStore()
 
     store.addBook("Veronica", "Salminen")
 
     assert len(store.books) == 1
+
+# Testar att markera bok som favorit
+def test_toggle_favorite():
+    store = BookStore()
+
+    store.addBook("Veronica", "Salminen")
+
+    store.toggleFavorite(0)
+
+    assert store.books[0]["favorite"] is True
